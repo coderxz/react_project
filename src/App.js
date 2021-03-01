2c2a44d45c0fbca4c6f32ac34239a7b0a850b3a3
尹召 <925612247@qq.com>
2021-03-01 15:34:52 +0800

    第二次提交!

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import Login from './pages/login/login';
-import Admin from './pages/admin/admin';
-import {Route,Switch} from 'react-router-dom';
-export default class App extends React.Component{
+import React,{Component} from 'react';
+export default class App extends Component{
   render() {
-    return (
-      <div className="app">
-        <Switch>
-          <Route path="/login" component={Login}/>
-          <Route path="/admin" component={Admin}/>
-        </Switch>
+    return(
+      <div>
+        App
       </div>
     )
   }
- }
-
+}
